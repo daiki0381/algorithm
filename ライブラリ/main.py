89db1d86a16d9576d67dcc09b080801c8c29610d
daiki0381 <daiki03810218@gmail.com>
2023-01-03 21:49:24 +0900

    add code

--- ライブラリ/main.py
+++ ライブラリ/main.py
@@ -75,11 +75,14 @@ list(product([0, 1], repeat=n))
 import itertools
 
 list = [0, 1, 2]
-print(list(itertools.permutations(list))) # => [(0, 1, 2), (0, 2, 1), (1, 0, 2), (1, 2, 0), (2, 0, 1), (2, 1, 0)]
+print(
+    list(itertools.permutations(list))
+)  # => [(0, 1, 2), (0, 2, 1), (1, 0, 2), (1, 2, 0), (2, 0, 1), (2, 1, 0)]
 
 # 二分探索
 
 from typing import List, Optional
+
 
 def binary_search(numbers: List[int], value: int) -> Optional[int]:
     left, right = 0, len(numbers) - 1
@@ -92,6 +95,7 @@ def binary_search(numbers: List[int], value: int) -> Optional[int]:
         else:
             right = mid - 1
     return None
+
 
 numbers = [0, 1, 5, 7, 9, 11, 15, 20, 24]
 print(binary_search(numbers, 15))  # => 6
@@ -110,6 +114,7 @@ print(cumulative_sum_list)  # => [1, 3, 6, 10, 15, 21, 28, 36, 45, 55]
 # 約数 => ある整数をわり切ることができる整数
 # 例) 30 => 1、2、3、5、6、10、15、30
 
+
 def make_divisors(n):
     divisors = []
     for i in range(1, int(n**0.5) + 1):
@@ -120,11 +125,13 @@ def make_divisors(n):
     divisors.sort()
     return divisors
 
+
 print(make_divisors(30))  # => [1, 2, 3, 5, 6, 10, 15, 30]
 
 # 素因数分解
 # 自然数を素数の積になるよう分解すること
 # 例) 30 => 2、3、5
+
 
 def make_prime_factorization_list(n):
     prime_factorization_list = []
@@ -142,6 +149,7 @@ def make_prime_factorization_list(n):
         prime_factorization_list.append(n)
     return prime_factorization_list
 
+
 print(make_prime_factorization_list(30))  # => [2, 3, 5]
 
 # 最大公約数
@@ -150,7 +158,7 @@ print(make_prime_factorization_list(30))  # => [2, 3, 5]
 
 import math
 
-print(math.gcd(144, 264, 84, 276)) # => 12
+print(math.gcd(144, 264, 84, 276))  # => 12
 
 # 最小公倍数
 # 倍数 => ある整数を何倍かした数
@@ -159,11 +167,12 @@ print(math.gcd(144, 264, 84, 276)) # => 12
 
 import math
 
-print(math.lcm(27, 9, 3)) # => 27
+print(math.lcm(27, 9, 3))  # => 27
 
 # n以下の素数全列挙
 # 素数 => 1より大きい自然数のうち、1とその数でしか割り切れないもの
 # エラトステネスの篩
+
 
 def primes(n):
     is_prime = [True] * (n + 1)
@@ -176,11 +185,13 @@ def primes(n):
             is_prime[j] = False
     return [i for i in range(n + 1) if is_prime[i]]
 
-print(primes(50)) # => [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47]
+
+print(primes(50))  # => [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47]
 
 # 素数判定
 
 import math
+
 
 def is_prime(n):
     sqrt_n = math.ceil(math.sqrt(n))
@@ -189,7 +200,8 @@ def is_prime(n):
             return False
     return True
 
-print(is_prime(53)) # => True
+
+print(is_prime(53))  # => True
 
 # 組み合わせ (重複なし)
 
@@ -228,7 +240,9 @@ print(list(enumerate([50, 60, 70])))  # => [(0, 50), (1, 60), (2, 70)]
 
 # 2次元配列内の1次元配列のn番目の要素に注目してソート
 
-print(sorted([[0, 3], [1, 1], [2, 4], [3, 15], [4, 9]], key=lambda x: x[1])) # => [[1, 1], [0, 3], [2, 4], [4, 9], [3, 15]]
+print(
+    sorted([[0, 3], [1, 1], [2, 4], [3, 15], [4, 9]], key=lambda x: x[1])
+)  # => [[1, 1], [0, 3], [2, 4], [4, 9], [3, 15]]
 
 # 二次元平面上の2点間の距離
 
@@ -237,7 +251,7 @@ import math
 A = (1, 2)
 B = (3, 4)
 
-print(math.sqrt((B[0] - A[0]) ** 2 + (B[1] - A[1]) ** 2)) # => 2.8284271247461903
+print(math.sqrt((B[0] - A[0]) ** 2 + (B[1] - A[1]) ** 2))  # => 2.8284271247461903
 
 # 二次元平面上で原点を中心に反時計回りに回転させた座標
 
@@ -260,20 +274,20 @@ print(math.sqrt(2))  # => 1.4142135623730951
 
 # 部分文字列
 
-print("aa" in "aabbcc") # => True
+print("aa" in "aabbcc")  # => True
 
 # 1の位
 
-print(1234 % 10) # => 4
+print(1234 % 10)  # => 4
 
 # 進数の変換
 
-print(int("1011", 2)) # => 11
+print(int("1011", 2))  # => 11
 
 # Mまでの総和
 
 M = 10
-print(M * (M + 1) // 2) # => 55
+print(M * (M + 1) // 2)  # => 55
 
 # forループをN-1から逆順
 
@@ -300,5 +314,5 @@ while q:
 """
 0 5
 1 4
-2 3 
+2 3
 """
