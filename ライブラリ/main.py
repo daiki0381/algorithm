fbcbccfff29db5a1fe3bc7dbb5789d8ace0ab5db
daiki0381 <daiki03810218@gmail.com>
2023-01-04 16:39:22 +0900

    add code

--- ライブラリ/main.py
+++ ライブラリ/main.py
@@ -281,6 +281,33 @@ for pattern in patterns:
 
 print(max(sum_values))
 
+# ナップサック問題 (動的計画法)
+# 問 ) https://atcoder.jp/contests/dp/tasks/dp_d
+# 計算量は表の縦 * 横となる => 今回の問題の制約は1≤N≤100 and 1≤W≤10 ** 5なので、100 * 10 ** 5=10 ** 7となり間に合う
+
+N, W = map(int, input().split())
+wv = [list(map(int, input().split())) for _ in range(N)]
+dp = [[0] * (W + 1) for _ in range(N + 1)]
+
+# 1番目
+for i in range(W + 1):
+    if i >= wv[0][0]:
+        dp[1][i] = wv[0][1]
+
+# 2番目以降
+for i in range(2, N + 1):
+    for j in range(W + 1):
+        no_choice = dp[i - 1][j]
+        if j < wv[i - 1][0]:
+            dp[i][j] = no_choice
+        else:
+            # 1つ上の段のi番目のw分戻った列のv+i番目のv
+            choice = dp[i - 1][j - wv[i - 1][0]] + wv[i - 1][1]
+            dp[i][j] = max(no_choice, choice)
+
+print(dp[N][W])
+
+
 # 組み合わせ (重複なし)
 
 from itertools import combinations
