97716fd67520b4a4320210b3feeabdb732e4ddef
daiki0381 <daiki03810218@gmail.com>
2023-01-05 19:05:43 +0900

    add code

--- ライブラリ/main.py
+++ ライブラリ/main.py
@@ -307,6 +307,40 @@ for i in range(2, N + 1):
 
 print(dp[N][W])
 
+# BFS (幅優先探索)
+# 全部回りたい時に使用する
+# https://atcoder.jp/contests/abc277/tasks/abc277_c
+
+from collections import deque, defaultdict
+
+N = int(input())
+d = defaultdict(list)
+
+# ①ある頂点から行ける頂点を格納したリストを生成
+for _ in range(N):
+    a, b = map(int, input().split())
+    d[a].append(b)
+    d[b].append(a)
+
+# ②キューを用意
+que = deque()
+que.append(1)
+
+# ③訪問チェックを用意
+seen = set()
+seen.add(1)
+
+# ④ 訪問後にque,seenに格納
+while que:
+    now = que.popleft()
+
+    for i in d[now]:
+        if i not in seen:
+            que.append(i)
+            seen.add(i)
+
+print(max(seen))
+
 
 # 組み合わせ (重複なし)
 
