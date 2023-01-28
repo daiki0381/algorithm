33ad643fc96432e0c057b3328828849588b80e49
daiki0381 <daiki03810218@gmail.com>
2023-01-28 23:57:53 +0900

    add code

--- ライブラリ/main.py
+++ ライブラリ/main.py
@@ -602,3 +602,42 @@ print(convert_alpha_to_num("ALL"))  # => 1000
 S = input()
 if S == S[::-1]:
     print("回文")
+
+# パスグラフ判定
+
+from collections import deque, defaultdict
+
+N, M = map(int, input().split())
+d = defaultdict(list)
+degree_1 = 0
+degree_2 = 0
+connected_component_num = 0
+
+for _ in range(M):
+    a, b = map(int, input().split())
+    d[a].append(b)
+    d[b].append(a)
+
+for k, v in d.items():
+    if len(v) == 1:
+        degree_1 += 1
+    elif len(v) == 2:
+        degree_2 += 1
+
+que = deque()
+seen = set()
+for i in range(1, N + 1):
+    if i not in seen:
+        connected_component_num += 1
+        que.append(i)
+        while que:
+            now = que.popleft()
+            for i in d[now]:
+                if i not in seen:
+                    que.append(i)
+                    seen.add(i)
+
+if degree_1 == 2 and degree_2 == N - 2 and connected_component_num == 1:
+    print("Yes")
+else:
+    print("No")
